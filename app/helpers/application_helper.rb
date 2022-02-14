33b340c447cd8dc70c8f63ac2c933d01d05e83f7
Adeynack <Adeynack@users.noreply.github.com>
2022-02-15 00:25:52 +0100

    fix div by zero bug
    + set Berlin as timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def human_hours_minutes(duration_in_hours)
-    div_mod = duration_in_hours.divmod(duration_in_hours <=> 0)
+    div_mod = duration_in_hours.divmod(duration_in_hours.negative? ? -1 : 1)
     div_mod.map!(&:-@) if duration_in_hours.negative?
     hours, decimal_minutes = div_mod
     minutes = (60 * decimal_minutes).round
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module HarvestMonitor
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Berlin"
     config.beginning_of_week = :monday
     # config.eager_load_paths << Rails.root.join("extras")
 
