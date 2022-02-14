7f3b0fa68e629025b8bcacc14a820d6c3b2d6860
Adeynack <Adeynack@users.noreply.github.com>
2022-02-14 19:03:55 +0100

    add time left

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem "jsbundling-rails"
 gem "turbo-rails"
 gem "stimulus-rails"
 gem "cssbundling-rails"
+gem "autoprefixer-rails"
 gem "jbuilder"
 # gem "redis", "~> 4.0" # Use Redis adapter to run Action Cable in production
 # gem "kredis" # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
   def human_hours_minutes(duration_in_hours)
-    hours, decimal_minutes = duration_in_hours.divmod(1)
+    div_mod = duration_in_hours.divmod(duration_in_hours <=> 0)
+    div_mod.map!(&:-@) if duration_in_hours.negative?
+    hours, decimal_minutes = div_mod
     minutes = (60 * decimal_minutes).round
     [hours, format("%02d", minutes)].join(":")
   end
