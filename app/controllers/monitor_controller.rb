43ccac0a982f285855e4ba95d7ec0797a61a9f37
Adeynack <Adeynack@users.noreply.github.com>
2022-05-04 14:29:10 +0200

    error handling & partial-ization

--- app/controllers/monitor_controller.rb
+++ app/controllers/monitor_controller.rb
@@ -16,25 +16,32 @@ class MonitorController < ApplicationController
     @worked_days_count = @worked_days.size
     @worked_days_so_far = @worked_days.count { |d| !d.future? }
 
-    harvest = HarvestApi.new
-    @user = Rails.cache.fetch([:harvest, :me, harvest.user_id], expires_in: 10.minutes) { harvest.me }
-    entries_for_this_week = Rails.cache.fetch([:harvest, :time_entries, harvest.user_id], expires_in: 5.minutes) do
-      harvest.time_entries(from: @first_day, to: @last_day)
+    @harvest_error = nil
+    begin
+      harvest = HarvestApi.new
+      @user = Rails.cache.fetch([:harvest, :me, harvest.user_id], expires_in: 10.minutes) { harvest.me }
+      entries_for_this_week = Rails.cache.fetch([:harvest, :time_entries, harvest.user_id], expires_in: 5.minutes) do
+        harvest.time_entries(from: @first_day, to: @last_day)
+      end
+    rescue StandardError => e
+      @harvest_error = e
     end
 
-    @daily_summaries = entries_for_this_week.group_by(&:spent_date).transform_values do |entries|
-      {
-        total_worked: entries.map(&:hours).sum,
-        total_billable: entries.filter(&:billable).map(&:hours).sum
+    unless @harvest_error
+      @daily_summaries = entries_for_this_week.group_by(&:spent_date).transform_values do |entries|
+        {
+          total_worked: entries.map(&:hours).sum,
+          total_billable: entries.filter(&:billable).map(&:hours).sum
+        }
+      end
+
+      @sprint_summary = {
+        total_worked: entries_for_this_week.map(&:hours).sum,
+        total_billable: entries_for_this_week.filter(&:billable).map(&:hours).sum
       }
+
+      @estimated_end_of_day = DateTime.current + (@worked_hours_per_day - @daily_summaries.dig(Date.current, :total_worked).to_d).hours
     end
-
-    @sprint_summary = {
-      total_worked: entries_for_this_week.map(&:hours).sum,
-      total_billable: entries_for_this_week.filter(&:billable).map(&:hours).sum
-    }
-
-    @estimated_end_of_day = DateTime.current + (@worked_hours_per_day - @daily_summaries.dig(Date.current, :total_worked).to_d).hours
 
     @seconds_to_reload = params.fetch(:seconds_to_reload, "60").to_i
 
